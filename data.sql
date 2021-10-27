18bc0260b3f12b5d88aeeed927d52da73c80c25d
xvnns <katya.krivokoneva@gmail.com>
2021-10-28 02:07:53 +0400

    data.sql file created

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `multi-functional-chat`.`role` (`id`, `name`) VALUES ('1', 'ADMIN');
+INSERT INTO `multi-functional-chat`.`role` (`id`, `name`) VALUES ('2', 'MODERATOR');
+INSERT INTO `multi-functional-chat`.`role` (`id`, `name`) VALUES ('3', 'USER');
